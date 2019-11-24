68eba2d35f3c7173eb96c4f95be5ed7029ebb48b
Smit <sthakkar1407@gmail.com>
2019-11-23 19:47:45 -0800

    truncate train data to 50K

--- data/downstream/file_truncate.py
+++ data/downstream/file_truncate.py
@@ -1,9 +1,31 @@
 import os, sys
+
+n = 50000
 readFile = open("./SNLI/s1.train")
 lines = readFile.readlines()
 
 readFile.close()
-w = open("./SNLI/ss1.train", 'w')
+w = open("./SNLI/s1_50k.train", 'w')
 
-w.writelines([items for items in lines[:50000]])
+w.writelines([items for items in lines[:n]])
 w.close()
+
+readFile = open("./SNLI/s2.train")
+lines = readFile.readlines()
+
+readFile.close()
+w = open("./SNLI/s2_50k.train", 'w')
+
+w.writelines([items for items in lines[:n]])
+w.close()
+
+readFile = open("./SNLI/labels.train")
+lines = readFile.readlines()
+
+readFile.close()
+w = open("./SNLI/labels_50k.train", 'w')
+
+w.writelines([items for items in lines[:n]])
+w.close()
+
+
